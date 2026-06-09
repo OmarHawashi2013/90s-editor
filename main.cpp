04a99cc0104b8a73642f451ed98d9e9e5b5f65c4
Omar Hawash <omarhawashprog2013@gmail.com>
2026-06-09 19:47:34 +0300

    Update main.cpp
    i added a save system

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 /*  
-    ================================================
-    =   /----\  \       /|  ------  |-----|        =
-    =  |      | | \    / | /      \ |      |       =
-    =  |      | |  \  /  | |------| |-----|        =
-    =  |      | |   \/   | |      | |     \        =
-    =  \ ____ / |        | |      | |      \       =
-    =  [ --> OFFSET: 0x0000 | STATUS: OK ]         =
-    =                                              =
-    =  [ OMAR ENGINE - Custom Win32 Framework ]    =
-    =  [ Developed by: Omar Hawash | (C) 2026 ]    =
-    ================================================
+	================================================
+	=   /----\  \       /|  ------  |-----|        =
+	=  |      | | \    / | /      \ |      |       =
+	=  |      | |  \  /  | |------| |-----|        =
+	=  |      | |   \/   | |      | |     \        =
+	=  \ ____ / |        | |      | |      \       =
+	=  [ --> OFFSET: 0x0000 | STATUS: OK ]         =
+	=                                              =
+	=  [ OMAR ENGINE - Custom Win32 Framework ]    =
+	=  [ Developed by: Omar Hawash | (C) 2026 ]    =
+	================================================
 
 */ 
 
@@ -20,6 +20,7 @@
 #include <string>
 #include <cstring>
 #include <chrono>
+#include <commdlg.h>
 #include "inc/gap.h"
 using namespace std;
 
@@ -41,52 +42,52 @@ HBRUSH hbrush_2 = CreateSolidBrush(RGB(195, 195, 195));
 int lines_num = 1;
 
 int hline(HDC backDC) {
-    string num_txt = "";
+	string num_txt = "";
 
-    for (int i = 0; i < lines_num; i++){
-        num_txt += to_string(i) + '\n';
-    }
+	for (int i = 0; i < lines_num; i++){
+		num_txt += to_string(i) + '\n';
+	}
 
-    SetTextColor(backDC, RGB(0, 0, 0));
-    RECT rect = {0, 0, 32, screenY};
+	SetTextColor(backDC, RGB(0, 0, 0));
+	RECT rect = {0, 0, 32, screenY};
 
-    DrawText(backDC, num_txt.c_str(), -1, &rect, DT_LEFT | DT_TOP);
+	DrawText(backDC, num_txt.c_str(), -1, &rect, DT_LEFT | DT_TOP);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
-    switch (uMsg) 
-    {
-        case WM_SIZE: {
-            int width = LOWORD(lParam);
-            int height = HIWORD(lParam);
+	switch (uMsg) 
+	{
+		case WM_SIZE: {
+			int width = LOWORD(lParam);
+			int height = HIWORD(lParam);
 
-            if (width > 0 && height > 0) {
-                DeleteObject(backBMP);
+			if (width > 0 && height > 0) {
+				DeleteObject(backBMP);
 
-                HDC hdc = GetDC(hwnd);
-                backBMP = CreateCompatibleBitmap(hdc, width, height);
-                SelectObject(backDC, backBMP);
-                ReleaseDC(hwnd, hdc);
+				HDC hdc = GetDC(hwnd);
+				backBMP = CreateCompatibleBitmap(hdc, width, height);
+				SelectObject(backDC, backBMP);
+				ReleaseDC(hwnd, hdc);
 
-                screenX = width;
-                screenY = height;
-            }
+				screenX = width;
+				screenY = height;
+			}
 
-            break;
-        }
-    	case WM_DESTROY:
-            DeleteObject(hbrush_1);
-            DeleteObject(hbrush_2);
-            DeleteObject(backBMP);
-            DeleteDC(backDC);
+			break;
+		}
+		case WM_DESTROY:
+			DeleteObject(hbrush_1);
+			DeleteObject(hbrush_2);
+			DeleteObject(backBMP);
+			DeleteDC(backDC);
 
-            PostQuitMessage(0);
-            return 0;
+			PostQuitMessage(0);
+			return 0;
  
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam); 
-    }
-    return 0; 
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam); 
+	}
+	return 0; 
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow){
@@ -99,167 +100,206 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow){
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindowEx(
-	    0,
-	    "90s editor",
-	    "90s editor",
-	    WS_OVERLAPPEDWINDOW,
+		0,
+		"90s editor",
+		"90s editor",
+		WS_OVERLAPPEDWINDOW,
 
-	    CW_USEDEFAULT, CW_USEDEFAULT, screenX, screenY,
+		CW_USEDEFAULT, CW_USEDEFAULT, screenX, screenY,
 
-	    NULL,
-	    NULL,
-	    hInstance,
-	    NULL
+		NULL,
+		NULL,
+		hInstance,
+		NULL
 	);
 
 	if (hwnd == NULL){
-	    return 0;
+		return 0;
 	}
 
 	ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+	UpdateWindow(hwnd);
 
-    HDC hdc = GetDC(hwnd);
+	HDC hdc = GetDC(hwnd);
 
-    backDC = CreateCompatibleDC(hdc);
-    backBMP = CreateCompatibleBitmap(hdc, screenX, screenY);
-    SelectObject(backDC, backBMP);
+	backDC = CreateCompatibleDC(hdc);
+	backBMP = CreateCompatibleBitmap(hdc, screenX, screenY);
+	SelectObject(backDC, backBMP);
 
-    /*x
-    init
-    */
+	/*x
+	init
+	*/
 
-    /*
-    init
-    */
+	AddFontResourceEx("W95F.otf", FR_PRIVATE, 0);
 
-
-    MSG msg;
-
-    while (true){
-        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
-            if(msg.message == WM_QUIT) return msg.wParam;
-
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        auto frameStart = std::chrono::high_resolution_clock::now();
-
-        if (GetForegroundWindow() == hwnd) {
-            for (int i = 0; i < 255; i++) {
-                if (GetAsyncKeyState(i) & 0x0001) {
-                    if (i == VK_BACK) {
-                        if (!v.empty() && v.size() > selector) {
-                            if (v.get_char_at(v.size() - selector - 1) == '\n') {
-                                lines_num --;
-                            }
-                            v.erase(v.size() - selector - 1);
-                        }
-                    }
-                    else if (i == VK_RETURN) {
-                        v.insert(v.size() - selector, '\n');
-                        lines_num ++;
-                    }
-                    else if (i == VK_TAB) {
-                        for (int i = 0; i < 8; i++) {
-                            v.insert(v.end() - selector, ' ');
-                        }
-                    }
-                    else if (i == VK_LEFT) {
-                        if (selector < v.size()) {
-                            selector ++;
-                        }
-                    }
-                    else if (i == VK_RIGHT) {
-                        if (selector > 0) {
-                            selector --;
-                        }
-                    }
-                    else if (i == VK_UP && v.size() - selector > 0) {
-                        for (int i = v.size() - selector - 1; i >= 0; i--) {
-                            if (v.get_char_at(i) == '\n') {
-                                selector = v.size() - i;
-
-                                break;
-                            }
-                        }
-                    }
-                    else if (i == VK_DOWN && v.size() - selector > 0) {
-                        for (int i = v.size() - selector; i < v.size(); i++) {
-                            if (v.get_char_at(i) == '\n') {
-                                selector = v.size() - (i + 1);
-
-                                break;
-                            }
-                        }
-                    }
-                    else if (i == VK_END && v.size() - selector + 1 > 0) {
-                        int target_pos = v.size();
-
-                        for (int i = v.size() - selector; i < v.size(); i ++) {
-                            if (v.get_char_at(i) == '\n') {
-                                target_pos = i;
-                                break;
-                            }
-                        }
-
-                        selector = v.size() - target_pos;
-                    }
-                    else if (i == VK_HOME && v.size() - selector > 0) {
-                        int target_pos = 0;
-
-                        for (int i = v.size() - selector - 1; i > 0; i --) {
-                            if (v.get_char_at(i) == '\n') {
-                                target_pos = i + 1;
-                                break;
-                            }
-                        }
-
-                        selector = v.size() - target_pos;
-                    }
-                    else {
-                        WORD char_buf[2] = {0};
-
-                        BYTE keyState[256];
-                        GetKeyboardState(keyState);
-
-                        int result = ToAscii(i, MapVirtualKey(i, MAPVK_VK_TO_VSC), keyState, char_buf, 0);
-
-                        if (result == 1) {
-                            v.insert(v.end() - selector, char_buf[0]);
-                        }
-                    }
-                }
-            }
-        }
-
-        SelectObject(backDC, hbrush_1);
-        Rectangle(backDC, 32, 0, screenX, screenY);
-
-        SelectObject(backDC, hbrush_2);
-        Rectangle(backDC, 0, 0, 32, screenY);
-
-        SetTextColor(backDC, RGB(0, 0, 0));
-        SetBkMode(backDC, TRANSPARENT);
-        if(!v.empty()) {
-            string s = v.to_string();
-            size_t pos = s.size() - selector;
-            s.insert(pos, "|");
-            RECT rect = { 32, 0, screenX, screenY };
-            DrawText(backDC, s.c_str(), -1, &rect, DT_LEFT | DT_TOP);
-        }
-
-        hline(backDC);
+	/*
+	init
+	*/
 
 
-        BitBlt(hdc, 0, 0, screenX, screenY, backDC, 0, 0, SRCCOPY);
+	MSG msg;
 
-        auto frameEnd = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> frameTime = frameEnd - frameStart;
+	while (true){
+		while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+			if(msg.message == WM_QUIT) return msg.wParam;
 
-        if (frameTime.count() < MS_PER_FRAME) {
-            Sleep(static_cast<DWORD>(MS_PER_FRAME - frameTime.count()));
-        }
-    }
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		auto frameStart = std::chrono::high_resolution_clock::now();
+
+		if (GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState('S') & 0x8000) {
+		    wchar_t szFileName[MAX_PATH] = L"";
+		    OPENFILENAMEW ofn = { 0 };
+		    ofn.lStructSize = sizeof(ofn);
+		    ofn.hwndOwner = hwnd;
+		    ofn.lpstrFilter = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+		    ofn.lpstrFile = szFileName;
+		    ofn.nMaxFile = MAX_PATH;
+		    ofn.Flags = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST;
+
+		    if (GetSaveFileNameW(&ofn)) {
+		        HANDLE hFile = CreateFileW(
+		            szFileName, 
+		            GENERIC_WRITE, 
+		            0, 
+		            NULL, 
+		            CREATE_ALWAYS, 
+		            FILE_ATTRIBUTE_NORMAL, 
+		            NULL
+		        );
+
+		        if (hFile != INVALID_HANDLE_VALUE) {
+		            DWORD dwBytesWritten = 0;
+
+		            std::string content = v.to_string(); 
+		            
+		            unsigned char bom[] = {0xEF, 0xBB, 0xBF};
+		            WriteFile(hFile, bom, 3, &dwBytesWritten, NULL);
+
+		            WriteFile(hFile, content.c_str(), (DWORD)content.length(), &dwBytesWritten, NULL);
+		            
+		            CloseHandle(hFile);
+		        }
+		    }
+		    Sleep(200); 
+		}
+
+		if (GetForegroundWindow() == hwnd and !(GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState('S') & 0x8000)) {
+			for (int i = 0; i < 255; i++) {
+				if (GetAsyncKeyState(i) & 0x0001) {
+					if (i == VK_BACK) {
+						if (!v.empty() && v.size() > selector) {
+							if (v.get_char_at(v.size() - selector - 1) == '\n') {
+								lines_num --;
+							}
+							v.erase(v.size() - selector - 1);
+						}
+					}
+					else if (i == VK_RETURN) {
+						v.insert(v.size() - selector, '\n');
+						lines_num ++;
+					}
+					else if (i == VK_TAB) {
+						for (int i = 0; i < 8; i++) {
+							v.insert(v.end() - selector, ' ');
+						}
+					}
+					else if (i == VK_LEFT) {
+						if (selector < v.size()) {
+							selector ++;
+						}
+					}
+					else if (i == VK_RIGHT) {
+						if (selector > 0) {
+							selector --;
+						}
+					}
+					else if (i == VK_UP && v.size() - selector > 0) {
+						for (int i = v.size() - selector - 1; i >= 0; i--) {
+							if (v.get_char_at(i) == '\n') {
+								selector = v.size() - i;
+
+								break;
+							}
+						}
+					}
+					else if (i == VK_DOWN && v.size() - selector > 0) {
+						for (int i = v.size() - selector; i < v.size(); i++) {
+							if (v.get_char_at(i) == '\n') {
+								selector = v.size() - (i + 1);
+
+								break;
+							}
+						}
+					}
+					else if (i == VK_END && v.size() - selector + 1 > 0) {
+						int target_pos = v.size();
+
+						for (int i = v.size() - selector; i < v.size(); i ++) {
+							if (v.get_char_at(i) == '\n') {
+								target_pos = i;
+								break;
+							}
+						}
+
+						selector = v.size() - target_pos;
+					}
+					else if (i == VK_HOME && v.size() - selector > 0) {
+						int target_pos = 0;
+
+						for (int i = v.size() - selector - 1; i > 0; i --) {
+							if (v.get_char_at(i) == '\n') {
+								target_pos = i + 1;
+								break;
+							}
+						}
+
+						selector = v.size() - target_pos;
+					}
+					else {
+						WORD char_buf[2] = {0};
+
+						BYTE keyState[256];
+						GetKeyboardState(keyState);
+
+						int result = ToAscii(i, MapVirtualKey(i, MAPVK_VK_TO_VSC), keyState, char_buf, 0);
+
+						if (result == 1) {
+							v.insert(v.end() - selector, char_buf[0]);
+						}
+					}
+				}
+			}
+		}
+
+		SelectObject(backDC, hbrush_1);
+		Rectangle(backDC, 32, 0, screenX, screenY);
+
+		SelectObject(backDC, hbrush_2);
+		Rectangle(backDC, 0, 0, 32, screenY);
+
+		SetTextColor(backDC, RGB(0, 0, 0));
+		SetBkMode(backDC, TRANSPARENT);
+		if(!v.empty()) {
+			string s = v.to_string();
+			size_t pos = s.size() - selector;
+			s.insert(pos, "|");
+			RECT rect = { 32, 0, screenX, screenY };
+			DrawText(backDC, s.c_str(), -1, &rect, DT_LEFT | DT_TOP);
+		}
+
+		hline(backDC);
+
+
+		BitBlt(hdc, 0, 0, screenX, screenY, backDC, 0, 0, SRCCOPY);
+
+		auto frameEnd = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::milli> frameTime = frameEnd - frameStart;
+
+		if (frameTime.count() < MS_PER_FRAME) {
+			Sleep(static_cast<DWORD>(MS_PER_FRAME - frameTime.count()));
+		}
+	}
 }
